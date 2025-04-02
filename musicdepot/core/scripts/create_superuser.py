ac7667beeb0be319db5ca818bd1db8a87c612522
Devarsh <devarsh12092003@gmail.com>
2025-04-02 16:28:53 -0400

    chore: udpate create superuser script

--- musicdepot/core/scripts/create_superuser.py
+++ musicdepot/core/scripts/create_superuser.py
@@ -1,5 +1,13 @@
 from django.contrib.auth import get_user_model
+
 User = get_user_model()
 
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser("admin", "admin@example.com", "adminpass123")
+if not User.objects.filter(username="admin@example.com").exists():
+    User.objects.create_superuser(
+        username="admin@example.com",
+        email="admin@example.com",
+        password="securepassword123"
+    )
+    print("✅ Superuser created.")
+else:
+    print("✅ Superuser already exists.")
