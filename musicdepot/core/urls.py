8daafe1106c149b55439f4f306a753a9a988b0ba
Devarsh <devarsh12092003@gmail.com>
2025-04-02 16:39:49 -0400

    feat: update paths

--- musicdepot/core/urls.py
+++ musicdepot/core/urls.py
@@ -1,7 +1,13 @@
+# musicdepot/urls.py
+
+from django.contrib import admin
 from django.urls import path
+from django.shortcuts import redirect  # Required for root redirect
 from .views import client_list, instructor_list
 
 urlpatterns = [
+    path('', lambda request: redirect('/admin/', permanent=False)),  # Root -> /admin/
+    path('admin/', admin.site.urls),
     path('clients/', client_list, name='client-list'),
     path('instructors/', instructor_list, name='instructor-list'),
 ]
